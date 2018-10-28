b74f6c3bf51801719ed5efab2b7772e8ccd7a5df
Rick Spencer <richard.linger.spencer.3@gmail.com>
2018-10-28 10:21:50 -0400

    got first iteration of generating form and adding data working

--- lib/persisted_model_form.dart
+++ lib/persisted_model_form.dart
@@ -3,24 +3,40 @@ import 'package:a2s_widgets/persisted_model.dart';
 
 class PersistedModelForm extends StatelessWidget {
   final PersistedModel model;
-  final _formKey = GlobalKey<FormState>();
-
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final Map<String, dynamic> _newData = {};
   PersistedModelForm(this.model);
 
+  List<Widget> _buildFormFields() {
+
+    List<Widget> fields = [];
+    model.labels.keys.forEach((String label) {
+      fields.add(
+        TextFormField(
+          decoration: InputDecoration(labelText: label),
+          onSaved: (String value){
+            _newData[model.labels[label]] = value;
+          }
+        ),
+      );
+    });
+    fields.add(RaisedButton(child: Text("Add"), onPressed: _submitForm));
+    return fields;
+  }
+
+  void _submitForm() {
+    _formKey.currentState.save();
+    model.add(_newData);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Add Form")),
       body: Form(
-          key: _formKey,
-          child: ListView(children: [
-            TextFormField(
-              initialValue: "A",
-            ),
-            TextFormField(
-              initialValue: "B",
-            ),
-          ])),
+        key: _formKey,
+        child: ListView(children: _buildFormFields()),
+      ),
     );
   }
+
 }
