16e3d7dee5738361e24d3e7edaa009d7092afa36
Rick Spencer <richard.linger.spencer.3@gmail.com>
2018-10-31 07:04:21 -0400

    added typed input field sniped from https://medium.com/saugo360/creating-custom-form-fields-in-flutter-85a8f46c2f41

--- lib/typed_input_field.dart
+++ lib/typed_input_field.dart
@@ -5,23 +5,56 @@ class TypedInputField extends StatelessWidget {
   final String label;
   final Function onSaved;
 
-  TypedInputField(this.fieldName, {@required this.label, @required this.onSaved});
+  TypedInputField(this.fieldName,
+      {@required this.label, @required this.onSaved});
 
   @override
   Widget build(BuildContext context) {
-    if(fieldName.toLowerCase().endsWith("count")){
-    return TextFormField(
-              decoration: InputDecoration(labelText: label),
-              keyboardType: TextInputType.number,
-              onSaved: (String value) {
-                int v = int.parse(value);
-                this.onSaved(v);
-              });
+    if (fieldName.toLowerCase().endsWith("count")) {
+      return Row(
+        children: [
+          Text(label),
+          IntegerFormField(onSaved: onSaved),
+        ],
+      );
     }
     return TextFormField(
-              decoration: InputDecoration(labelText: label),
-              onSaved: (String value) {
-                this.onSaved(value);
-              });
+        decoration: InputDecoration(labelText: label),
+        onSaved: (String value) {
+          this.onSaved(value);
+        });
   }
+}
+
+class IntegerFormField extends FormField<int> {
+  IntegerFormField(
+      {FormFieldSetter<int> onSaved,
+      FormFieldValidator<int> validator,
+      int initialValue = 0,
+      bool autovalidate = false})
+      : super(
+            onSaved: onSaved,
+            validator: validator,
+            initialValue: initialValue,
+            autovalidate: autovalidate,
+            builder: (FormFieldState<int> state) {
+              return Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.remove),
+                    onPressed: () {
+                      state.didChange(state.value - 1);
+                    },
+                  ),
+                  Text(state.value.toString()),
+                  IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: () {
+                      state.didChange(state.value + 1);
+                    },
+                  ),
+                ],
+              );
+            });
 }
