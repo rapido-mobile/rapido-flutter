55d3e0e017c56affe26eaf797a2b88ad356d54d1
Rick Spencer <richard.linger.spencer.3@gmail.com>
2018-12-01 09:48:26 -0500

    bones of map-point picker dialog in place

--- lib/src/map_point_form_field.dart
+++ lib/src/map_point_form_field.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-// import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapPointFormField extends StatefulWidget {
   /// The name of the field, used to calculate which type of input to return
@@ -23,17 +24,20 @@ class MapPointFormField extends StatefulWidget {
 class _MapPointFormFieldState extends State<MapPointFormField> {
   @override
   Widget build(BuildContext context) {
-    print("--------------");
-    print(widget.initialValue);
-
-    print(
-        "${widget.initialValue["longitude"]},${widget.initialValue["latitude"]}");
     return TextFormField(
         decoration: InputDecoration(
           labelText: widget.label,
           suffixIcon: IconButton(
             icon: Icon(Icons.map),
-            onPressed: () {},
+            onPressed: () {
+              showDialog<Map<String, double>>(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return MapPointDialog();
+                  }).then((Map<String, double> location) {
+                print(location);
+              });
+            },
           ),
         ),
         initialValue:
@@ -41,5 +45,88 @@ class _MapPointFormFieldState extends State<MapPointFormField> {
         onSaved: (String value) {
           widget.onSaved(value);
         });
+  }
+}
+
+class MapPointDialog extends StatefulWidget {
+  final Map<String, double> initialValue;
+  MapPointDialog({this.initialValue});
+
+  _MapPointDialogState createState() => _MapPointDialogState();
+}
+
+class _MapPointDialogState extends State<MapPointDialog> {
+  Map<String, double> _startingMapPoint;
+  Widget awaitingWidget = Center(child: CircularProgressIndicator());
+  GoogleMapController mapController;
+
+  @override
+  void initState() {
+    if (widget.initialValue != null) {
+      _startingMapPoint = widget.initialValue;
+    } else {
+      _setCurrentLocation();
+    }
+    super.initState();
+  }
+
+  void _setCurrentLocation() async {
+    Location().getLocation().then((Map<String, double> location) {
+      setState(() {
+        _startingMapPoint = location;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: _startingMapPoint == null
+          ? awaitingWidget
+          : Column(
+              children: <Widget>[
+                Icon(Icons.map),
+                Text(""),
+                SizedBox(
+                  width: 300.0,
+                  height: 300.0,
+                  child: Overlay(initialEntries: [
+                    OverlayEntry(builder: (BuildContext context) {
+                      return GoogleMap(
+                        options: new GoogleMapOptions(
+                          trackCameraPosition: true,
+                          myLocationEnabled: true,
+                          cameraPosition: new CameraPosition(
+                              target: new LatLng(_startingMapPoint["latitude"],
+                                  _startingMapPoint["longitude"]),
+                              zoom: 15.0),
+                        ),
+                        onMapCreated: (GoogleMapController controller) {
+                          mapController = controller;
+                        },
+                      );
+                    }),
+                    OverlayEntry(builder: (BuildContext context) {
+                      return Icon(Icons.flag,
+                          color: Theme.of(context).accentColor);
+                    })
+                  ]),
+                ),
+                FloatingActionButton(
+                  child: Icon(Icons.check),
+                  onPressed: () {
+                    Map<String, double> mp = {
+                      "latitude": mapController.cameraPosition.target.latitude,
+                      "longitude":
+                          mapController.cameraPosition.target.longitude,
+                    };
+                    Navigator.pop(
+                      context, mp
+                    );
+                  },
+                )
+              ],
+            ),
+    );
   }
 }
