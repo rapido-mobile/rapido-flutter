a2da753208a63ee78225744865ec0c7bc02f0ef3
Rick Spencer <richard.linger.spencer.3@gmail.com>
2018-12-28 16:32:18 -0500

    added DocumentListPage class to fix #88

---
+++ lib/src/document_list_page_view.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:rapido/rapido.dart';
+
+class DocumentListPageView extends StatefulWidget {
+  final DocumentList documentList;
+  DocumentListPageView(this.documentList);
+  _DocumentListPageViewState createState() => _DocumentListPageViewState();
+}
+
+class _DocumentListPageViewState extends State<DocumentListPageView> {
+    DocumentList data;
+
+  @override
+  initState() {
+    super.initState();
+    data = widget.documentList;
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    widget.documentList.onChanged = (DocumentList newData) {
+      setState(() {
+        data = newData;
+      });
+    };
+    if (!widget.documentList.documentsLoaded) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    return PageView.builder(
+        itemCount: widget.documentList.length,
+        itemBuilder: (BuildContext context, int i) {
+          return DocumentPage(
+            document: widget.documentList[i],
+            labels: widget.documentList.labels,
+          );
+        });
+  }
+}
